33d01bc7f2646b8a8a53dea6e2835510a5ea2a45
Andrew Carter <andrew@podium-network.com>
2019-01-03 18:37:33 +0000

    Made default endpoint return basic app config JSON.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
 
+var fs = require('fs');
+var conf = fs.readFileSync('config.json', 'utf8');
+var Config = JSON.parse(conf);
+
 var counter = 0;
 setInterval(
 	() => { counter = counter + 1 },
 	1000
 )
+
+app.get('/', (_, res) => {
+	res.send(conf)
+})
 
 app.get('/', (req, res) => {
   res.send("There have been " + counter + " seconds since activation...")
