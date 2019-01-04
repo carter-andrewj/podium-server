756e7114051b6e14f1c17be05882ca5db827d5ac
Andrew Carter <andrew@podium-network.com>
2019-01-04 16:38:16 +0000

    Setting expressJS access control.

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 const express = require('express')
 const app = express()
 
-var fs = require('fs');
-var conf = fs.readFileSync('config.json', 'utf8');
-var Config = JSON.parse(conf);
+var fs = require('fs')
+var conf = fs.readFileSync('config.json', 'utf8')
+var Config = JSON.parse(conf)
 
-var counter = 0;
-setInterval(
-	() => { counter = counter + 1 },
-	1000
-)
+// Check if podium is already set up for this network/app-key
 
-app.get('/', (_, res) => {
+// Set up root podium account
+
+// Set up rulebook entries
+
+// Launch smart contracts
+
+// Launch bots
+
+
+// Set express access control middleware
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+// Default (ping) route
+app.get("/", (_, res) => { res.send(200) })
+
+// Config route
+app.get("/config", (_, res) => {
 	res.send(conf)
 })
 
-app.get('/', (req, res) => {
-  res.send("There have been " + counter + " seconds since activation...")
-})
+// User listing route
+
+// User search route
+
+// Topic listing route
+
+// Topic search route
+
+// Media endpoint route
+// app.put('/media', (req, res) => {
+// 	// Check media object has been registered with ledger
+// 	// Upload image to S3
+// 	res.send(200)
+// })
+
 
 app.listen(3000, () => console.log('Podium Server running on port 3000'))
