598e3a3326b9bd53d3bd7088de131fbf901b3c1d
Andrew Carter <andrew@podium-network.com>
2019-01-05 19:33:30 +0000

    Redeploying with HTTPS.

--- index.js
+++ index.js
@@ -52,4 +52,3 @@ app.get("/config", (_, res) => {
 // })
 
 app.listen(3000, () => console.log('Podium Server running on port 3000'))
-//app.listen(3001, () => console.log('Podium Server running on port 3001'))
