b78003071f418c8649abb6a675df8d9898a26b5c
Andrew Carter <andrew@podium-network.com>
2019-01-07 16:13:39 +0000

    Trying to get CORS to work with any locahost port.

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ config.launched = (new Date).getTime()
 // Set express access control middleware
 //TODO - Make this more secure than just allowing everything through
 app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Origin', "http://" + req.hostname);
+	res.header('Access-Control-Allow-Origin', "http://" + req.headers.origin);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
     next();
